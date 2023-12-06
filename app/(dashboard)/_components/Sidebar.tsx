11a05f69c5a2183caf4adbbfa6c7f47a4f498cec
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-12-06 21:18:22 +0200

    feat: create layout and sidebar fro dashboard page

---
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+    return (
+        <div className="flex h-full flex-col overflow-y-auto border-r bg-white shadow-sm">
+            <div className="p-6">
+                <Logo />
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
