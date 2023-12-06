78b593df6f8532d052e8953195dbb1c4af5ec855
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-12-07 00:22:07 +0200

    fix: layout updated

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,18 @@
+import Navbar from './_components/Navbar';
 import Sidebar from './_components/Sidebar';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="h-full">
+            {/* Navbar */}
+            <div className="fixed inset-y-0 z-50 h-[80px] w-full md:pl-56">
+                <Navbar />
+            </div>
             {/* Sidebar */}
             <div className="fixed inset-y-0 z-50 hidden h-full w-56 flex-col md:flex">
                 <Sidebar />
             </div>
-            {children}
+            <main className="h-full md:pl-56">{children}</main>
         </div>
     );
 };
