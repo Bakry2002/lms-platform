c762e7e3f596c329ccb465b13a26621f2dea0bbe
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-12-06 23:14:28 +0200

    feat: add sidebar items

--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -1,9 +1,15 @@
+import Logo from './Logo';
+import SidebarRoutes from './SidebarRoutes';
+
 const Sidebar = () => {
     return (
         <div className="flex h-full flex-col overflow-y-auto border-r bg-white shadow-sm">
+            {/* Logo */}
             <div className="p-6">
                 <Logo />
             </div>
+            {/* Sidebar routes */}
+            <SidebarRoutes />
         </div>
     );
 };
---
+++ app/(dashboard)/_components/SidebarItem.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { LucideIcon } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
+
+type SidebarItemProps = {
+    label: string;
+    icon: LucideIcon;
+    href: string;
+};
+
+const SidebarItem = ({ label, icon: Icon, href }: SidebarItemProps) => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isActive =
+        (pathname === '/' && href === '/') ||
+        pathname === href ||
+        pathname?.startsWith(`${href}/`);
+    // active path if the current pathname is the root path
+    // or the current pathname is the same as the href
+    // or the current pathname starts with the href sub path of specific route
+
+    const onClick = () => {
+        router.push(href);
+    };
+
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            className={cn(
+                'flex items-center gap-x-2 pl-6 text-sm font-[500] text-slate-500 transition-all hover:bg-slate-300/20 hover:text-slate-600',
+                isActive &&
+                    'bg-sky-200/20 text-sky-700 hover:bg-sky-200/20 hover:text-sky-700',
+            )}
+        >
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon
+                    size={22}
+                    className={cn('text-slate-500', isActive && 'text-sky-700')}
+                />
+            </div>
+        </button>
+    );
+};
+
+export default SidebarItem;
