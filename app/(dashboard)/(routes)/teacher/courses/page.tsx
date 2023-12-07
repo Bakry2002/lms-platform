93127ff9dd53e41972d9fbb47b30196d4ea65c40
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-12-07 14:20:06 +0200

    install axios & shdacn-ui components

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import { Button, buttonVariants } from '@/components/ui/button';
+import Link from 'next/link';
+
 const CoursesPage = () => {
-    return <div>Courses Page </div>;
+    return (
+        <div className="p-6">
+            <Link href="/teacher/create" className={buttonVariants()}>
+                New Course
+            </Link>
+        </div>
+    );
 };
 
 export default CoursesPage;
---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const CreateCoursePage = () => {
+    return <div>CreateCoursePage</div>;
+};
+
+export default CreateCoursePage;
