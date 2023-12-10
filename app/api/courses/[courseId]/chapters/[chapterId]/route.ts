db44e1bdcd01f22bee190038b1780536916062f6
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-12-10 15:14:39 +0200

    edit: add the mux video to the chapter db

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,13 @@
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
+import Mux from '@mux/mux-node';
+
+// initiate mux
+const { Video } = new Mux(
+    process.env.MUX_TOKEN_ID!,
+    process.env.MUX_TOKEN_SECRET!,
+);
 
 export async function PATCH(
     req: Request,
@@ -43,7 +50,38 @@ export async function PATCH(
             },
         });
 
-        // TODO: Handle video upload
+        if (values.videoUrl) {
+            // find existing mux data for this chapter, delete the asset if it already exists and create a new one
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId,
+                },
+            });
+
+            if (existingMuxData) {
+                await Video.Assets.del(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id,
+                    },
+                });
+            }
+
+            // if no existing mux data, create a new one
+            const asset = await Video.Assets.create({
+                input: values.videoUrl,
+                playback_policy: 'public',
+                test: false,
+            });
+
+            await db.muxData.create({
+                data: {
+                    chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id,
+                },
+            });
+        }
 
         return NextResponse.json(chapter);
     } catch (error) {
