f2603d4797a7ffe726ffd311db29d6c232486b4b
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-12-12 01:02:47 +0200

    update: finalize server action for get chapter

--- actions/get-chapter.ts
+++ actions/get-chapter.ts
@@ -51,8 +51,57 @@ export const getChapter = async ({
         let nextChapter: Chapter | null = null;
 
         // we will get this above if the user purchased the course
-        if(purchase)
-        
+        if (purchase) {
+            // load attachments
+            attachments = await db.attachment.findMany({
+                where: {
+                    courseId,
+                },
+            });
+        }
+
+        if (chapter.isFree || purchase) {
+            // get the mux data (video) if the chapter is free or the user purchased the course
+            muxData = await db.muxData.findUnique({
+                where: {
+                    chapterId,
+                },
+            });
+
+            nextChapter = await db.chapter.findFirst({
+                where: {
+                    courseId,
+                    isPublished: true,
+                    position: {
+                        gt: chapter?.position, // the position of the current chapter is less than the next chapter
+                    },
+                },
+                orderBy: {
+                    position: 'asc',
+                },
+            });
+        }
+
+        // get user progress for this chapter
+        const userProgress = await db.userProgress.findUnique({
+            where: {
+                userId_chapterId: {
+                    userId,
+                    chapterId,
+                },
+            },
+        });
+
+        // return the data
+        return {
+            chapter,
+            course,
+            muxData,
+            attachments,
+            nextChapter,
+            userProgress,
+            purchase,
+        };
     } catch (error) {
         console.log('[ERROR] getChapter: ', error);
         return {
